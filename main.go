59f0221ce0e4ad9e60f3b3c568c03221ebf10941
tillepille <github@tillepille.io>
2021-07-24 21:32:22 +0200

    update source

--- main.go
+++ main.go
@@ -15,10 +15,13 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
-	"github.com/doneland/yquotes"
+	"github.com/go-resty/resty/v2"
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -50,6 +53,215 @@ var (
 	)
 )
 
+func transformResponseQuote(responseQuote ResponseQuote) Quote {
+
+	if responseQuote.MarketState == "REGULAR" {
+		return Quote{
+			ResponseQuote:           responseQuote,
+			Price:                   responseQuote.RegularMarketPrice,
+			PricePrevClose:          responseQuote.RegularMarketPreviousClose,
+			PriceOpen:               responseQuote.RegularMarketOpen,
+			PriceDayHigh:            responseQuote.RegularMarketDayHigh,
+			PriceDayLow:             responseQuote.RegularMarketDayLow,
+			Change:                  (responseQuote.RegularMarketChange),
+			ChangePercent:           responseQuote.RegularMarketChangePercent,
+			IsActive:                true,
+			IsRegularTradingSession: true,
+			IsVariablePrecision:     false,
+			CurrencyConverted:       "USD",
+			Name:                    responseQuote.ShortName,
+			Symbol:                  responseQuote.Symbol,
+		}
+	}
+
+	if responseQuote.MarketState == "POST" && responseQuote.PostMarketPrice == 0.0 {
+		return Quote{
+			ResponseQuote:           responseQuote,
+			Price:                   responseQuote.RegularMarketPrice,
+			PricePrevClose:          responseQuote.RegularMarketPreviousClose,
+			PriceOpen:               responseQuote.RegularMarketOpen,
+			PriceDayHigh:            responseQuote.RegularMarketDayHigh,
+			PriceDayLow:             responseQuote.RegularMarketDayLow,
+			Change:                  (responseQuote.RegularMarketChange),
+			ChangePercent:           responseQuote.RegularMarketChangePercent,
+			IsActive:                true,
+			IsRegularTradingSession: false,
+			IsVariablePrecision:     false,
+			CurrencyConverted:       "USD",
+			Name:                    responseQuote.ShortName,
+			Symbol:                  responseQuote.Symbol,
+		}
+	}
+
+	if responseQuote.MarketState == "PRE" && responseQuote.PreMarketPrice == 0.0 {
+		return Quote{
+			ResponseQuote:           responseQuote,
+			Price:                   responseQuote.RegularMarketPrice,
+			PricePrevClose:          responseQuote.RegularMarketPreviousClose,
+			PriceOpen:               responseQuote.RegularMarketOpen,
+			PriceDayHigh:            responseQuote.RegularMarketDayHigh,
+			PriceDayLow:             responseQuote.RegularMarketDayLow,
+			Change:                  (responseQuote.RegularMarketChange),
+			ChangePercent:           responseQuote.RegularMarketChangePercent,
+			IsActive:                false,
+			IsRegularTradingSession: false,
+			IsVariablePrecision:     false,
+			CurrencyConverted:       "USD",
+			Name:                    responseQuote.ShortName,
+			Symbol:                  responseQuote.Symbol,
+		}
+	}
+
+	if responseQuote.MarketState == "POST" {
+		return Quote{
+			ResponseQuote:           responseQuote,
+			Price:                   responseQuote.PostMarketPrice,
+			PricePrevClose:          responseQuote.RegularMarketPreviousClose,
+			PriceOpen:               responseQuote.RegularMarketOpen,
+			PriceDayHigh:            responseQuote.RegularMarketDayHigh,
+			PriceDayLow:             responseQuote.RegularMarketDayLow,
+			Change:                  (responseQuote.PostMarketChange + responseQuote.RegularMarketChange),
+			ChangePercent:           responseQuote.PostMarketChangePercent + responseQuote.RegularMarketChangePercent,
+			IsActive:                true,
+			IsRegularTradingSession: false,
+			IsVariablePrecision:     false,
+			CurrencyConverted:       "USD",
+			Name:                    responseQuote.ShortName,
+			Symbol:                  responseQuote.Symbol,
+		}
+	}
+
+	if responseQuote.MarketState == "PRE" {
+		return Quote{
+			ResponseQuote:           responseQuote,
+			Price:                   responseQuote.PreMarketPrice,
+			PricePrevClose:          responseQuote.RegularMarketPreviousClose,
+			PriceOpen:               responseQuote.RegularMarketOpen,
+			PriceDayHigh:            responseQuote.RegularMarketDayHigh,
+			PriceDayLow:             responseQuote.RegularMarketDayLow,
+			Change:                  (responseQuote.PreMarketChange),
+			ChangePercent:           responseQuote.PreMarketChangePercent,
+			IsActive:                true,
+			IsRegularTradingSession: false,
+			IsVariablePrecision:     false,
+			CurrencyConverted:       "USD",
+			Name:                    responseQuote.ShortName,
+			Symbol:                  responseQuote.Symbol,
+		}
+	}
+
+	if responseQuote.PostMarketPrice != 0.0 {
+		return Quote{
+			ResponseQuote:           responseQuote,
+			Price:                   responseQuote.PostMarketPrice,
+			PricePrevClose:          responseQuote.RegularMarketPreviousClose,
+			PriceOpen:               responseQuote.RegularMarketOpen,
+			PriceDayHigh:            responseQuote.RegularMarketDayHigh,
+			PriceDayLow:             responseQuote.RegularMarketDayLow,
+			Change:                  (responseQuote.PostMarketChange + responseQuote.RegularMarketChange),
+			ChangePercent:           responseQuote.PostMarketChangePercent + responseQuote.RegularMarketChangePercent,
+			IsActive:                false,
+			IsRegularTradingSession: false,
+			IsVariablePrecision:     false,
+			CurrencyConverted:       "USD",
+			Name:                    responseQuote.ShortName,
+			Symbol:                  responseQuote.Symbol,
+		}
+	}
+
+	return Quote{
+		ResponseQuote: responseQuote,
+
+		Price:                   responseQuote.RegularMarketPrice,
+		PricePrevClose:          responseQuote.RegularMarketPreviousClose,
+		PriceOpen:               responseQuote.RegularMarketOpen,
+		PriceDayHigh:            responseQuote.RegularMarketDayHigh,
+		PriceDayLow:             responseQuote.RegularMarketDayLow,
+		Change:                  (responseQuote.RegularMarketChange),
+		ChangePercent:           responseQuote.RegularMarketChangePercent,
+		IsActive:                false,
+		IsRegularTradingSession: false,
+		IsVariablePrecision:     false,
+		CurrencyConverted:       "USD",
+		Name:                    responseQuote.ShortName,
+		Symbol:                  responseQuote.Symbol,
+	}
+
+}
+
+type ResponseQuote struct {
+	ShortName                  string  `json:"shortName"`
+	Symbol                     string  `json:"symbol"`
+	MarketState                string  `json:"marketState"`
+	Currency                   string  `json:"currency"`
+	ExchangeName               string  `json:"fullExchangeName"`
+	ExchangeDelay              float64 `json:"exchangeDataDelayedBy"`
+	RegularMarketChange        float64 `json:"regularMarketChange"`
+	RegularMarketChangePercent float64 `json:"regularMarketChangePercent"`
+	RegularMarketPrice         float64 `json:"regularMarketPrice"`
+	RegularMarketPreviousClose float64 `json:"regularMarketPreviousClose"`
+	RegularMarketOpen          float64 `json:"regularMarketOpen"`
+	RegularMarketDayRange      string  `json:"regularMarketDayRange"`
+	RegularMarketDayHigh       float64 `json:"regularMarketDayHigh"`
+	RegularMarketDayLow        float64 `json:"regularMarketDayLow"`
+	RegularMarketVolume        float64 `json:"regularMarketVolume"`
+	PostMarketChange           float64 `json:"postMarketChange"`
+	PostMarketChangePercent    float64 `json:"postMarketChangePercent"`
+	PostMarketPrice            float64 `json:"postMarketPrice"`
+	PreMarketChange            float64 `json:"preMarketChange"`
+	PreMarketChangePercent     float64 `json:"preMarketChangePercent"`
+	PreMarketPrice             float64 `json:"preMarketPrice"`
+	FiftyTwoWeekHigh           float64 `json:"fiftyTwoWeekHigh"`
+	FiftyTwoWeekLow            float64 `json:"fiftyTwoWeekLow"`
+	QuoteType                  string  `json:"quoteType"`
+	MarketCap                  float64 `json:"marketCap"`
+}
+
+type Quote struct {
+	ResponseQuote
+	Price                   float64
+	PricePrevClose          float64
+	PriceOpen               float64
+	PriceDayHigh            float64
+	PriceDayLow             float64
+	Change                  float64
+	ChangePercent           float64
+	IsActive                bool
+	IsRegularTradingSession bool
+	IsVariablePrecision     bool
+	CurrencyConverted       string
+	Name                    string
+	Symbol                  string
+}
+
+type Response struct {
+	QuoteResponse struct {
+		Quotes []ResponseQuote `json:"result"`
+		Error  interface{}     `json:"error"`
+	} `json:"quoteResponse"`
+}
+
+func GetQuotes(symbols []string) []Quote {
+	client := resty.New()
+	symbolsString := strings.Join(symbols, ",")
+	url := fmt.Sprintf("https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com&symbols=%s", symbolsString)
+	res, _ := client.R().
+		SetResult(Response{}).
+		Get(url)
+
+	return transformResponseQuotes((res.Result().(*Response)).QuoteResponse.Quotes)
+}
+
+func transformResponseQuotes(responseQuotes []ResponseQuote) []Quote {
+
+	quotes := make([]Quote, 0)
+	for _, responseQuote := range responseQuotes {
+		quotes = append(quotes, transformResponseQuote(responseQuote))
+	}
+	return quotes
+
+}
+
 func init() {
 	// register the collector metrics in the default
 	// registry.
@@ -73,6 +285,8 @@ func (c collector) Describe(ch chan<- *prometheus.Desc) {
 }
 
 func (c collector) Collect(ch chan<- prometheus.Metric) {
+	stocks := make([]string, 0)
+
 	for _, s := range c {
 		if s == "" {
 			// should never happen
@@ -83,63 +297,35 @@ func (c collector) Collect(ch chan<- prometheus.Metric) {
 		if glog.V(2) {
 			glog.Infof("looking up %s\n", s)
 		}
+		stocks = append(stocks, s)
+	}
 
-		start := time.Now()
-		stock, err := yquotes.NewStock(s, false)
-		queryDuration.Observe(float64(time.Since(start).Seconds()))
+	start := time.Now()
+	quoteList := GetQuotes(stocks)
+	queryDuration.Observe(float64(time.Since(start).Seconds()))
+	for _, singleStock := range quoteList {
 
-		if err != nil {
-			glog.Infof("error: %v\n", err)
-			errorCount.WithLabelValues(s).Inc()
-			continue
-		}
-
-		symbol := stock.Symbol
-		name := stock.Name
-		price := stock.Price
-
-		ls := []string{"symbol", "name"}
-		lvs := []string{symbol, name}
+		ls := []string{"symbol", "name", "active"}
+		lvs := []string{singleStock.Symbol, singleStock.Name, strconv.FormatBool(singleStock.IsActive)}
 
 		ch <- prometheus.MustNewConstMetric(
 			prometheus.NewDesc("yquotes_last_price_dollars", "Last price paid.", ls, nil),
 			prometheus.GaugeValue,
-			price.Last,
-			lvs...,
-		)
-
-		ch <- prometheus.MustNewConstMetric(
-			prometheus.NewDesc("yquotes_bid_price_dollars", "Bid price.", ls, nil),
-			prometheus.GaugeValue,
-			price.Bid,
-			lvs...,
-		)
-
-		ch <- prometheus.MustNewConstMetric(
-			prometheus.NewDesc("yquotes_ask_price_dollars", "Asking price.", ls, nil),
-			prometheus.GaugeValue,
-			price.Ask,
+			singleStock.Price,
 			lvs...,
 		)
 
 		ch <- prometheus.MustNewConstMetric(
 			prometheus.NewDesc("yquotes_opening_price_dollars", "Opening price.", ls, nil),
 			prometheus.GaugeValue,
-			price.Open,
+			singleStock.PriceOpen,
 			lvs...,
 		)
 
 		ch <- prometheus.MustNewConstMetric(
 			prometheus.NewDesc("yquotes_previous_close_price_dollars", "Previous close price.", ls, nil),
 			prometheus.GaugeValue,
-			price.PreviousClose,
-			lvs...,
-		)
-
-		ch <- prometheus.MustNewConstMetric(
-			prometheus.NewDesc("yquotes_publish_timestamp", "The time this data was published.", ls, nil),
-			prometheus.GaugeValue,
-			float64(price.Date.UnixNano())/float64(time.Second),
+			singleStock.PricePrevClose,
 			lvs...,
 		)
 	}
